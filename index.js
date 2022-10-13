c407442311c50e699f5b4c26fd992fc92f75423e
dgcezar <diego.cezar13@gmail.com>
2022-10-13 19:44:16 -0300

    atualização 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const jogo1 = {
-  nomeDoJogo: "Fallout 4".toUpperCase(),
+let jogo1 = {
+  titulo: "Fallout 4".toUpperCase(),
   genero: "RPG",
   descricao:
     "O jogador é o único sobrevivente do Vault 111 que emerge 210 anos após o dia da Grande Guerra. Antes disso,  ocorre um período de jogo durante a era pré-Guerra mostrando o jogador vivendo com sua esposa e filho.",
@@ -9,8 +9,8 @@ const jogo1 = {
   jogoDoAno: true,
 };
 
-const jogo2 = {
-  nomeDoJogo: "Divinity: Original Sin 2".toUpperCase(),
+let jogo2 = {
+  titulo: "Divinity: Original Sin 2".toUpperCase(),
   genero: "RPG",
   descricao:
     "Reúna seu grupo e prepare-se para uma aventura fantástica! Sua imaginação é o único limite na sua busca por restaurar do Divino e salvar o mundo. Combine os elementos para aniquilar seus inimigos. Use técnicas para subverter tudo no seu caminho. Seja você um elfo, humano, anão, lagarto ou morto-vivo, as pessoas vão saber das suas viagens e vão reagir aos seus feitos.",
@@ -26,8 +26,8 @@ const jogo2 = {
   jogoDoAno: false,
 };
 
-const jogo3 = {
-  nomeDoJogo: "Shadow of The Tomb Raider".toUpperCase(),
+let jogo3 = {
+  titulo: "Shadow of The Tomb Raider".toUpperCase(),
   genero: "Ação",
   descricao:
     "Em Shadow of the Tomb Raider Definitive Edition, acompanhe o capítulo final da origem de Lara e testemunhe sua     transformação na desbravadora que ela está destinada a ser. Combinando o jogo-base e todas as sete tumbas de    desafio de DLC, além de armas, roupas e habilidades extras, Shadow of the Tomb Raider Definitive Edition é a melhor    maneira de viver o momento decisivo da vida de Lara.",
@@ -44,18 +44,66 @@ const jogo3 = {
   precoAtual: 145.9,
   jogoDoAno: true,
 };
-
+// Criar array vázio
 const listaJogos = [];
 
+// Adicionar os objetos ao array vázio
 listaJogos.push(jogo1,jogo2,jogo3);
+console.log(listaJogos);
 
-// if (jogo1.jogoDoAno === true && jogo2.jogoDoAno === true && jogo3.jogoDoAno === true) {
-//   listaJogos.push(jogo1,jogo2,jogo3);
+// Tornar os array dos objetos em string
+// console.log(jogo1.plataforma.join(), jogo2.plataforma.join(), jogo3.plataforma.join())
+
+// Criar um laço para exibir os objetos
+// for(jogo of listaJogos){
+//   for (i in jogo){
+//       console.log(`${i}: ${jogo[i]}`)
+//   }
+// console.log("----------------------");
+// }
+
+// Criar uma função que receba como parâmetro um objeto e devolva a string do relatório com dados do objeto
+const relatorioJogo = (param) => {  
+  for(i in listaJogos){
+    if (param === listaJogos[i]) {
+      console.log(`${param.join()}: ${param}`)
+    }
+  }
+console.log("----------------------");
+
+}
+relatorioJogo(jogo1);
+
+
+// Adicionar condição de verdadeiro ao adicionar objetos na lista
+// if (jogo1.jogoDoAno === true) {
+//   listaJogos.push(jogo1);
 // } else {
 //   alert("Esse jogo não é válido");
-//   } 
+// }
+// if (jogo2.jogoDoAno === true) {
+//   listaJogos.push(jogo2);
+// } else {
+//   alert("Esse jogo não é válido");
+// }
+// if (jogo3.jogoDoAno === true) {
+//   listaJogos.push(jogo3);
+// } else {
+//   alert("Esse jogo não é válido");
+// }
 // console.log(listaJogos);
 
+
+
+// Cálculo de média
+// const mediaPreco = (jogo1.precoAtual + jogo2.precoAtual + jogo3.precoAtual) / 3;
+// console.log(mediaPreco);
+
+// // Verificação de booleanos
+// const saoJogosDoAno = jogo1.jogoDoAno && jogo2.jogoDoAno && jogo3.jogoDoAno;
+// console.log(saoJogosDoAno);
+
+// console.log(jogo1, jogo2, jogo3);
 
 // for (let jogo of jogos) {
 //   for (let propriedades in jogos) {
@@ -63,13 +111,4 @@ listaJogos.push(jogo1,jogo2,jogo3);
 //   }
 // }
 
-
 //jogos.jogoDoAno ? jogos.push("sim") : alert("Não pode ser adicionado");
-
-// const mediaPreco = (jogos[0].precoAtual + jogos[1].precoAtual + jogos[2].precoAtual) / 3;
-// console.log(mediaPreco);
-
-// const saoJogosDoAno = jogos[0].jogoDoAno && jogos[1].jogoDoAno && jogos[2].jogoDoAno;
-// console.log(saoJogosDoAno);
-
-// console.log(jogos.join(""))
